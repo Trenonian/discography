6bf7cc9e9ccd3c6f3711a5e61d098d72d67ddcb3
Michael Anderson <amon-ra@sbcglobal.net>
2014-10-07 14:30:25 -0500

    Added more robust hover functionality

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(".stemage").hover(
+    function () {
+        $("tr.stemage").toggleClass("highlight");
+        $(".band.stemage").toggleClass("showoutline");
+    }
+);
+
+$(".danimal").hover(
+    function () {
+        $("tr.danimal").toggleClass("highlight");
+        $(".band.danimal").toggleClass("showoutline");
+    }
+);
+$(".kirby").hover(
+    function () {
+        $("tr.kirby").toggleClass("highlight");
+        $(".band.kirby").toggleClass("showoutline");
+    }
+);
+$(".chunckstyle").hover(
+    function () {
+        $("tr.chunckstyle").toggleClass("highlight");
+        $(".band.chunckstyle").toggleClass("showoutline");
+    }
+);
+$(".cheddar").hover(
+    function () {
+        $("tr.cheddar").toggleClass("highlight");
+        $(".band.cheddar").toggleClass("showoutline");
+    }
+);
