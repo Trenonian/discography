2296a03c57695b2d6e1d560bb593c11893a3659c
Michael Anderson <amon-ra@sbcglobal.net>
2014-10-08 14:47:00 -0500

    added click events for band members

--- js/script.js
+++ js/script.js
@@ -1,31 +1,71 @@
+var hovering = "";
+
 $(".stemage").hover(
     function () {
-        $("tr.stemage").toggleClass("highlight");
-        $(".band.stemage").toggleClass("showoutline");
+        $("tr.stemage").addClass("highlight");
+        $(".band.stemage").addClass("showoutline");
+        hovering = "stemage";
+    },
+    function () {
+        $("tr.stemage").removeClass("highlight");
+        $(".band.stemage").removeClass("showoutline");
+        hovering = "";
     }
 );
 
 $(".danimal").hover(
     function () {
-        $("tr.danimal").toggleClass("highlight");
-        $(".band.danimal").toggleClass("showoutline");
+        $("tr.danimal").addClass("highlight");
+        $(".band.danimal").addClass("showoutline");
+        hovering = "danimal";
+    },
+    function () {
+        $("tr.danimal").removeClass("highlight");
+        $(".band.danimal").removeClass("showoutline");
+        hovering = "";
     }
 );
 $(".kirby").hover(
     function () {
-        $("tr.kirby").toggleClass("highlight");
-        $(".band.kirby").toggleClass("showoutline");
+        $("tr.kirby").addClass("highlight");
+        $(".band.kirby").addClass("showoutline");
+        hovering = "kirby";
+    },
+    function () {
+        $("tr.kirby").removeClass("highlight");
+        $(".band.kirby").removeClass("showoutline");
+        hovering = "";
     }
 );
 $(".chunckstyle").hover(
     function () {
-        $("tr.chunckstyle").toggleClass("highlight");
-        $(".band.chunckstyle").toggleClass("showoutline");
+        $("tr.chunckstyle").addClass("highlight");
+        $(".band.chunckstyle").addClass("showoutline");
+        hovering = "chunckstyle";
+    },
+    function () {
+        $("tr.chunckstyle").removeClass("highlight");
+        $(".band.chunckstyle").removeClass("showoutline");
+        hovering = "";
     }
 );
 $(".cheddar").hover(
     function () {
-        $("tr.cheddar").toggleClass("highlight");
-        $(".band.cheddar").toggleClass("showoutline");
+        $("tr.cheddar").addClass("highlight");
+        $(".band.cheddar").addClass("showoutline");
+        hovering = "cheddar";
+    },
+    function () {
+        $("tr.cheddar").removeClass("highlight");
+        $(".band.cheddar").removeClass("showoutline");
+        hovering = "";
     }
 );
+
+$(".imgwrapper, table").hover(function () {
+    $(this).click(function () {
+        if (hovering != "") {
+            console.log(hovering);
+        }
+    });
+});
